1bc9a7017138f2e1cec68d376f8d521a322a03da
jimmyalvarez <alejandro24alvarez@gmail.com>
2018-08-05 12:32:51 -0500

    first commit

--- Vagrantfile
+++ Vagrantfile
@@ -3,11 +3,13 @@
 
 Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/xenial64"
-  config.vm.network "forwarded_port", guest: 80, host: 8080
+  config.vm.network "forwarded_port", guest: 8080, host: 8080
   config.vm.network "forwarded_port", guest: 8000, host: 8000
   config.vm.provision "docker"
   config.vm.provision "shell" do |s|
     s.path = "provision.sh"
   end
-  
+  config.vm.provider "virtualbox" do |vb|
+      vb.memory = "4092"
+  end
 end
--- provision.sh
+++ provision.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
+sudo apt-get update  > /dev/null 2>&1
+
+########################
+#  Node
+########################
+#echo "Installing Node"
+#sudo apt-get install -y nodejs
+#sudo apt-get install -y npm
+
 ########################
 # Jenkins
 ########################
 echo "Installing Java"
-sudo apt-get -y install default-jre > /dev/null 2>&1
+sudo apt-get install openjdk-8-jdk
+#sudo apt-get -y install default-jre > /dev/null 2>&1
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
+
+########################
+# maven
+########################
+sudo apt-get -y install maven
 
 ########################
 # Jenkins
@@ -13,22 +29,32 @@ wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key 
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update > /dev/null 2>&1
 sudo apt-get -y install jenkins > /dev/null 2>&1
-sudo usermod -aG docker jenkins
+sudo usermod -aG jenkins docker
+Sudo service Jenkins restart
+
 ########################
 # nginx
 ########################
-echo "Installing nginx"
-sudo apt-get -y install nginx > /dev/null 2>&1
-sudo service nginx start
+#echo "installing node"
+#sudo apt-get update > /dev/null 2>&1
+#sudo apt-get install nodejs
+#sudo apt-get install npm
+
+########################
+# nginx
+########################
+#echo "Installing nginx"
+#sudo apt-get -y install nginx > /dev/null 2>&1
+#sudo service nginx start
 
 ########################
 # Configuring nginx
 ########################
-echo "Configuring nginx"
-cd /etc/nginx/sites-available
-sudo rm default ../sites-enabled/default
-sudo cp /vagrant/VirtualHost/jenkins /etc/nginx/sites-available/
-sudo ln -s /etc/nginx/sites-available/jenkins /etc/nginx/sites-enabled/
-sudo service nginx restart
-sudo service jenkins restart
-echo "Success"
+#echo "Configuring nginx"
+#cd /etc/nginx/sites-available
+#sudo rm default ../sites-enabled/default
+#sudo cp /vagrant/VirtualHost/jenkins /etc/nginx/sites-available/
+#sudo ln -s /etc/nginx/sites-available/jenkins /etc/nginx/sites-enabled/
+#sudo service nginx restart
+#sudo service jenkins restart
+#echo "Success"
